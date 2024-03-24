2e616cc1d534ad71d8c71d03a0a24f6a2a739afa
SFukunishi <fukunishi.s@tsunagu-grp.jp>
2024-03-24 17:28:17 +0900

    get latitude and longitude from predicted place list

--- lib/widgets/map_with_search_bar_widget.dart
+++ lib/widgets/map_with_search_bar_widget.dart
@@ -5,6 +5,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter_config/flutter_config.dart';
 import 'package:google_place/google_place.dart';
+import 'package:geocoding/geocoding.dart';
 
 class MapWithSearchBarWidget extends StatefulWidget {
   const MapWithSearchBarWidget({Key? key}) : super(key: key);
@@ -70,108 +71,129 @@ class _MapWithSearchBarWidgetState extends State<MapWithSearchBarWidget> {
   void autoCompleteSearch(String value) async {
     var result = await googlePlace.autocomplete.get(value);
     if (result != null && result.predictions != null && mounted) {
+      print('iiiii');
+      // print(searchPredictions[1].description.toString());
       setState(() {
         searchPredictions = result.predictions!;
+        print('aaaaaaa');
+        // print(searchPredictions[1].placeId);
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    var LatAndLng = [];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Map with Search Bar'),
       ),
-      body: Stack(
-        // child: Column(
-          children: [
-            Expanded(
-              // flex: 2,
-              child: GoogleMap(
-                mapType: MapType.normal,
-                initialCameraPosition: _kGooglePlex,
-                myLocationEnabled: true,
-                onMapCreated: (GoogleMapController controller) {
-                  mapController = controller;
-                },
-                onTap: (LatLng latLang) {
-                  print('Clicked: $latLang');
-                },
-              ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 5,
+            child: GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: _kGooglePlex,
+              myLocationEnabled: true,
+              onMapCreated: (GoogleMapController controller) {
+                mapController = controller;
+              },
+              onTap: (LatLng latLang) {
+                print('Clicked: $latLang');
+              },
             ),
-            Align(
-              alignment: Alignment.topCenter,
-              // flex: 3,
-              // child: SafeArea(
-              // child: Padding(
-              //   padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                      boxShadow: const [
-                        BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 10.0,
-                            spreadRadius: 1.0,
-                            offset: Offset(10, 10))
-                      ],
-                    ),
-                    child: TextFormField(
-                      onChanged: (value) {
-                        if (value.isNotEmpty) {
-                          autoCompleteSearch(value);
-                        } else {
-                          if (searchPredictions.length > 0 && mounted) {
-                            setState(() {
-                              searchPredictions = [];
-                            });
-                          }
-                        }
-                      },
-                      decoration: InputDecoration(
-                        prefixIcon: IconButton(
-                          color: Colors.grey[500],
-                          icon: const Icon(Icons.arrow_back_ios_new),
-                          onPressed: () {
-                            // Navigator.pop(context);
-                          },
-                        ),
-                        hintText: 'バッティングセンター',
-                        hintStyle: const TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.bold),
-                        border: InputBorder.none,
+          ),
+          Expanded(
+            flex: 4,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Container(
+                      width: screenWidth * 0.9,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: const [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 10.0,
+                              spreadRadius: 1.0,
+                              offset: Offset(10, 10))
+                        ],
                       ),
-                    ),
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: searchPredictions.length, // 検索結果の配列の長さを指定,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          child: ListTile(
-                            title: Text(searchPredictions[index]
-                                .description
-                                .toString()),
-                            onTap: () async {
-                              // Handle onTap
+                      child: TextFormField(
+                        onChanged: (value) {
+                          if (value.isNotEmpty) {
+                            autoCompleteSearch(value);
+                          } else {
+                            if (searchPredictions.length > 0 && mounted) {
+                              setState(() {
+                                searchPredictions = [];
+                              });
+                            }
+                          }
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: IconButton(
+                            color: Colors.grey[500],
+                            icon: const Icon(Icons.arrow_back_ios_new),
+                            onPressed: () {
+                              // Navigator.pop(context);
                             },
                           ),
-                        );
-                      },
+                          hintText: 'バッティングセンター',
+                          hintStyle: const TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
+                          border: InputBorder.none,
+                        ),
+                      ),
                     ),
-                  ),
-                ],
-                // ),
+                    Expanded(
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: searchPredictions.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            child: ListTile(
+                              title: Text(searchPredictions[index]
+                                  .description
+                                  .toString()),
+                              onTap: () async {
+                                List? locations = await locationFromAddress(
+                                    searchPredictions[index]
+                                        .description
+                                        .toString());
+                                if (locations != null && locations.isNotEmpty) {
+                                  print(locations.first.latitude);
+                                  print(locations.first.longitude);
+                                  // ここで必要な処理を実行する
+                                  setState(() {
+                                    // 取得した経度と緯度を配列に格納
+                                    LatAndLng.add(locations.first.latitude);
+                                    LatAndLng.add(locations.first.longitude);
+                                  });
+                                  print(LatAndLng);
+                                } else {
+                                  print('No location found for the address');
+                                }
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              // ),
             ),
-          ],
-        ),
-      // ),
+          ),
+        ],
+      ),
     );
   }
 }
